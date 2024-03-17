d7cd1fccb26d2755b71bdbf8270db6ba391d52e2
surajjayraman <suraj.jayraman@gmail.com>
2024-03-17 18:51:21 -0400

    Update TopBar

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "../globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import LeftSideBar from "@/components/layout/LeftSideBar";
+import TopBar from "@/components/layout/TopBar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
         <body className={inter.className}>
           <div className="flex max-lg:flex-col text-grey-1">
             <LeftSideBar />
+            <TopBar />
             <div className="flex-1">{children}</div>
           </div>
         </body>
--- components/layout/TopBar.tsx
+++ components/layout/TopBar.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import { UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+
+import { navLinks } from "@/lib/constants";
+import { Menu } from "lucide-react";
 
 const TopBar = () => {
   return (
-    <div className='sticky top-0'>TopBar</div>
-  )
-}
+    <div className="sticky top-0 z-20 w-full flex justify-between items-center px-8 py-4 b-blue-2 shadow-xl lg:hidden">
+      <Image src="/logo.png" alt="logo" width={150} height={70} />
+      <div className="flex gap-8 max-md:hidden">
+        {navLinks.map((link) => (
+          <Link
+            href={link.url}
+            key={link.label}
+            className="flex gap-4 text-body-medium"
+          >
+            {link.icon}
+            <p>{link.label}</p>
+          </Link>
+        ))}
+      </div>
+      <div className="flex gap-4 items-center">
+        <Menu className="cursor-pointer md:hidden" />
+        <UserButton />
+      </div>
+    </div>
+  );
+};
 
-export default TopBar
+export default TopBar;
