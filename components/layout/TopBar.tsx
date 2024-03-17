2f5f4f6151501431d195df542a2291b271f55e09
surajjayraman <suraj.jayraman@gmail.com>
2024-03-17 19:26:19 -0400

    Update TopBar drop down menu

--- components/layout/TopBar.tsx
+++ components/layout/TopBar.tsx
@@ -1,11 +1,15 @@
+"use client";
+
 import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 
 import { navLinks } from "@/lib/constants";
 import { Menu } from "lucide-react";
+import { useState } from "react";
 
 const TopBar = () => {
+  const [dropDownMenu, setDropDownMenu] = useState(false);
   return (
     <div className="sticky top-0 z-20 w-full flex justify-between items-center px-8 py-4 b-blue-2 shadow-xl lg:hidden">
       <Image src="/logo.png" alt="logo" width={150} height={70} />
@@ -20,8 +24,24 @@ const TopBar = () => {
           </Link>
         ))}
       </div>
-      <div className="flex gap-4 items-center">
-        <Menu className="cursor-pointer md:hidden" />
+      <div className="relative flex gap-4 items-center">
+        <Menu
+          className="cursor-pointer md:hidden"
+          onClick={() => setDropDownMenu(!dropDownMenu)}
+        />
+        {dropDownMenu && (
+          <div className="absolute top-10 right-6 flex flex-col gap-8 p-5 bg-white shadow-xl rounded-lg">
+            {navLinks.map((link) => (
+              <Link
+                href={link.url}
+                key={link.label}
+                className="flex gap-4 text-body-medium"
+              >
+                <p>{link.label}</p>
+              </Link>
+            ))}
+          </div>
+        )}
         <UserButton />
       </div>
     </div>
