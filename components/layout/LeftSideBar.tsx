f8d321a2e5f0feb8350f2eda41c9526cda3046a8
surajjayraman <suraj.jayraman@gmail.com>
2024-03-16 20:05:03 -0400

    Update LeftSideBar component

--- components/layout/LeftSideBar.tsx
+++ components/layout/LeftSideBar.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const LeftSideBar = () => {
-  return <div className="h-screen left-0 top-0 sticky p-10">leftSideBar</div>;
+  return (
+    <div className="h-screen left-0 top-0 sticky p-10 flex flex-col gap-16 bg-blue-2 shadow-xl max-lg:hidden">
+      leftSideBar
+    </div>
+  );
 };
 
 export default LeftSideBar;
