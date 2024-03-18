9b8dfcddf0e126f4c93ae6e3072001cb12e38521
surajjayraman <suraj.jayraman@gmail.com>
2024-03-17 22:05:09 -0400

    Add component CollectionForm

--- app/(dashboard)/collections/new/page.tsx
+++ app/(dashboard)/collections/new/page.tsx
@@ -1,6 +1,7 @@
+import CollectionForm from "@/components/collections/CollectionForm";
 
 const CreateCollection = () => {
-  return <div>CreateCollection</div>;
+  return <CollectionForm />;
 };
 
-export default CreateCollection
+export default CreateCollection;
---
+++ components/collections/CollectionForm.tsx
@@ -0,0 +1,5 @@
+const CollectionForm = () => {
+  return <div className="p-10">CollectionForm</div>;
+};
+
+export default CollectionForm;
