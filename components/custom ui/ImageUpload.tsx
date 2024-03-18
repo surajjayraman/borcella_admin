562f0caed56d5b530bc2ee54c62521e1891dceca
surajjayraman <suraj.jayraman@gmail.com>
2024-03-18 12:51:50 -0400

    Update ImageUpload onRemove

--- components/custom ui/ImageUpload.tsx
+++ components/custom ui/ImageUpload.tsx
@@ -1,5 +1,5 @@
 import { CldUploadWidget } from "next-cloudinary";
-import { Plus } from "lucide-react";
+import { Plus, Trash } from "lucide-react";
 
 import { Button } from "../ui/button";
 import Image from "next/image";
@@ -22,15 +22,25 @@ const ImageUpload: React.FC<ImageUploadProps> = ({
   return (
     <div>
       <div className="mb-4 flex flex-wrap items-center gap-4">
-        {value.map((url, index) => (
-          <Image
-            key={index}
-            src={url}
-            alt="collection"
-            className="object-cover rounded-lg"
-            width={200}
-            height={200}
-          />
+        {value.map((url) => (
+          <div key={url} className="relative w-[200px] h-[200px]">
+            <div className="absolute top-0 right-0 z-10">
+              <Button
+                type="button"
+                onClick={() => onRemove(url)}
+                size="sm"
+                className="bg-red-1 text-white"
+              >
+                <Trash className="h-4 w-4" />
+              </Button>
+            </div>
+            <Image
+              src={url}
+              alt="collection"
+              className="object-cover rounded-lg"
+              fill
+            />
+          </div>
         ))}
       </div>
 
