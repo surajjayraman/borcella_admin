7775c793aec759c957c19e87c3cf670477d54097
surajjayraman <suraj.jayraman@gmail.com>
2024-03-18 10:28:22 -0400

    Update CollectionForm with zod

--- components/collections/CollectionForm.tsx
+++ components/collections/CollectionForm.tsx
@@ -1,10 +1,18 @@
+"use client";
+
+import { z } from "zod";
+
 import { Separator } from "../ui/separator";
+
+const formSchema = z.object({
+  username: z.string().min(2).max(50),
+});
 
 const CollectionForm = () => {
   return (
     <div className="p-10">
-          <p className="text-heading2-bold">Create Collection</p>
-          <Separator className="bg-grey-1 mt-4 mb-7" />
+      <p className="text-heading2-bold">Create Collection</p>
+      <Separator className="bg-grey-1 mt-4 mb-7" />
     </div>
   );
 };
