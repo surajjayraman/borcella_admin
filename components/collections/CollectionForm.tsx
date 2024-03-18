6caa4cc651af81e1089d3a0208a8f8fe4761cac4
surajjayraman <suraj.jayraman@gmail.com>
2024-03-18 08:30:01 -0400

    Add shadcn separator to collection form

--- components/collections/CollectionForm.tsx
+++ components/collections/CollectionForm.tsx
@@ -1,5 +1,12 @@
+import { Separator } from "../ui/separator";
+
 const CollectionForm = () => {
-  return <div className="p-10">CollectionForm</div>;
+  return (
+    <div className="p-10">
+          <p className="text-heading2-bold">Create Collection</p>
+          <Separator className="bg-grey-1 mt-4 mb-7" />
+    </div>
+  );
 };
 
 export default CollectionForm;
