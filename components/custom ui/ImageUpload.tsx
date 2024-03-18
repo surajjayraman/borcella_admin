501c99e6b0a9cb581763b54d414ad091d1723699
surajjayraman <suraj.jayraman@gmail.com>
2024-03-18 12:21:34 -0400

    Update ImageUpload onUpload

--- components/custom ui/ImageUpload.tsx
+++ components/custom ui/ImageUpload.tsx
@@ -2,6 +2,7 @@ import { CldUploadWidget } from "next-cloudinary";
 import { Plus } from "lucide-react";
 
 import { Button } from "../ui/button";
+import Image from "next/image";
 
 interface ImageUploadProps {
   value: string[];
@@ -14,17 +15,36 @@ const ImageUpload: React.FC<ImageUploadProps> = ({
   onRemove,
   value,
 }) => {
+  const onUpload = (result: any) => {
+    onChange(result.info.secure_url);
+  };
+
   return (
-    <CldUploadWidget uploadPreset="ypgbzet3">
-      {({ open }) => {
-        return (
-          <Button className="bg-grey-1 text-white" onClick={() => open()}>
-            <Plus className="h-4 w-4 mr-2" />
-            Upload Image
-          </Button>
-        );
-      }}
-    </CldUploadWidget>
+    <div>
+      <div className="mb-4 flex flex-wrap items-center gap-4">
+        {value.map((url, index) => (
+          <Image
+            key={index}
+            src={url}
+            alt="collection"
+            className="object-cover rounded-lg"
+            width={200}
+            height={200}
+          />
+        ))}
+      </div>
+
+      <CldUploadWidget uploadPreset="ypgbzet3" onSuccess={onUpload}>
+        {({ open }) => {
+          return (
+            <Button className="bg-grey-1 text-white" onClick={() => open()}>
+              <Plus className="h-4 w-4 mr-2" />
+              Upload Image
+            </Button>
+          );
+        }}
+      </CldUploadWidget>
+    </div>
   );
 };
 
