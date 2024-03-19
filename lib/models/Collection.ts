b228926e3938016c73e07a6646ba3445fada63ce
surajjayraman <suraj.jayraman@gmail.com>
2024-03-19 12:33:06 -0400

    Update model Collection

---
+++ lib/models/Collection.ts
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const CollectionSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  description: String,
+  image: {
+    type: String,
+    required: true,
+  },
+  products: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+    },
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Collection =
+  mongoose.models.Collection || mongoose.model("Collection", CollectionSchema);
+
+export default Collection;
