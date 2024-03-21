c881d302abbe21e326861eebc7838c8d56dee03d
surajjayraman <suraj.jayraman@gmail.com>
2024-03-21 11:21:23 -0400

    Update Collections page styles

--- app/(dashboard)/collections/page.tsx
+++ app/(dashboard)/collections/page.tsx
@@ -1,6 +1,9 @@
 "use client";
 import { columns } from "@/components/collections/CollectionColumns";
 import { DataTable } from "@/components/custom ui/DataTable";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { Plus } from "lucide-react";
 import React, { useEffect, useState } from "react";
 
 const Collections = () => {
@@ -28,12 +31,16 @@ const Collections = () => {
   }, []);
 
   return (
-    <>
+    <div className="px-10 py-5">
       <div className="flex items-center justify-between">
         <p className="text-heading2-bold">Collections</p>
+        <Button className="bg-blue-1 text-white">
+          <Plus className="h4 w-4 mr-2" /> Create Collection
+        </Button>
       </div>
+      <Separator className="bg-grey-1 my-4" />
       <DataTable columns={columns} data={collections} />
-    </>
+    </div>
   );
 };
 
