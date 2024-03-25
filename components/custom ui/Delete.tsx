d3e7f390988f55d944df9a2a6bca16aa26d423c9
surajjayraman <suraj.jayraman@gmail.com>
2024-03-24 20:29:30 -0400

    Update Delete component with alert dialog

--- components/custom ui/Delete.tsx
+++ components/custom ui/Delete.tsx
@@ -1,11 +1,40 @@
 import { Trash } from "lucide-react";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+
 import { Button } from "../ui/button";
 
 const Delete = () => {
   return (
-    <Button className="bg-red-1 text-white">
-      <Trash className="h-4 w-4" />
-    </Button>
+    <AlertDialog>
+      <AlertDialogTrigger>
+        <Button className="bg-red-1 text-white">
+          <Trash className="h-4 w-4" />
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction>Continue</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
   );
 };
 
