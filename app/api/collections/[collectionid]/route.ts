0cb5185ed0ba2c0764fb7f8dedbaf8bd7e327bd5
surajjayraman <suraj.jayraman@gmail.com>
2024-03-24 21:23:33 -0400

    Update api route to Delete a collection

--- app/api/collections/[collectionid]/route.ts
+++ app/api/collections/[collectionid]/route.ts
@@ -1,10 +1,25 @@
 import { NextRequest, NextResponse } from "next/server";
 
+import { auth } from "@clerk/nextjs";
+import connectToDB from "@/lib/mongoDB";
+import Collection from "@/lib/models/Collection";
+
 export const DELETE = async (
   req: NextRequest,
   { params }: { params: { collectionId: string } }
 ) => {
   try {
+    const { userId } = auth();
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    await connectToDB();
+
+    await Collection.findByIdAndDelete(params.collectionId);
+
+    return new NextResponse("Collection is deleted", { status: 200 });
   } catch (error) {
     console.log("collectionId_DELETE", error);
     return new NextResponse(JSON.stringify(error), { status: 500 });
