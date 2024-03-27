a27308cc5461774a9327bdf12c3f675b9aa76e7c
surajjayraman <suraj.jayraman@gmail.com>
2024-03-26 20:26:30 -0400

    Update Page CollectionDetails

--- app/(dashboard)/collections/[collectionId]/page.tsx
+++ app/(dashboard)/collections/[collectionId]/page.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
+"use client";
 
-const CollectionDetails = () => {
-  return (
-    <div>CollectionDetails</div>
-  )
-}
+import { useEffect, useState } from "react";
 
-export default CollectionDetails
+const CollectionDetails = ({
+  params,
+}: {
+  params: { collectionId: string };
+}) => {
+  const [loading, setLoading] = useState(true);
+  const [collectionDetails, setCollectionDetails] =
+    useState<CollectionType | null>(null);
+
+  useEffect(() => {
+    const getCollectionDetails = async () => {
+      try {
+        const res = await fetch(`/api/collections/${params.collectionId}`, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+        const data = await res.json();
+        setCollectionDetails(data);
+        setLoading(false);
+      } catch (err) {
+        console.log("[CollectionDetails_getCollectionDetails]", err);
+      }
+    };
+
+    getCollectionDetails();
+  }, [params.collectionId]);
+
+  return <div>CollectionDetails</div>;
+};
+
+export default CollectionDetails;
