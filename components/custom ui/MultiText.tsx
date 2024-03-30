b0735930e5713e8394f8ae8d4ad8ce048e7a5486
surajjayraman <suraj.jayraman@gmail.com>
2024-03-30 10:30:32 -0400

    Update custom ui component MultiText addTag handler with badge

--- components/custom ui/MultiText.tsx
+++ components/custom ui/MultiText.tsx
@@ -1,11 +1,49 @@
+"use client";
+
+import { useState } from "react";
+import { Input } from "../ui/input";
+import { Badge } from "../ui/badge";
+
 interface MultiTextProps {
   placeholder: string;
   value: string[];
   onChange: (value: string) => void;
   onRemove: (value: string) => void;
 }
-const MultiText: React.FC<MultiTextProps> = ({placeholder, value, onChange, onRemove}) => {
-  return <div>MultiText</div>;
+const MultiText: React.FC<MultiTextProps> = ({
+  placeholder,
+  value,
+  onChange,
+  onRemove,
+}) => {
+  const [inputValue, setInputValue] = useState("");
+
+  const addTag = (item: string) => {
+    onChange(item);
+    setInputValue("");
+  };
+  return (
+    <>
+      <Input
+        placeholder={placeholder}
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            e.preventDefault();
+            addTag(inputValue);
+          }
+        }}
+      />
+      <div className="flex gap-1 flex-wrap mt-4">
+        {value.map((tag, index) => (
+          <Badge key={index} className="bg-grey-1 text-white">
+            {tag}
+          </Badge>
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default MultiText;
