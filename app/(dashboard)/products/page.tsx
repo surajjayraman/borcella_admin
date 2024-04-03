d12c3148320fba43abc0618d9669dfdb9b5714e4
surajjayraman <suraj.jayraman@gmail.com>
2024-04-02 20:03:43 -0400

    Update products page with getProducts

--- app/(dashboard)/products/page.tsx
+++ app/(dashboard)/products/page.tsx
@@ -1,6 +1,21 @@
-import React from "react";
+"use client";
+
+import { useEffect, useState } from "react";
 
 const Products = () => {
+  const [loading, setLoading] = useState(true);
+  const [products, setProducts] = useState<ProductType[] | null>([]);
+
+  useEffect(() => {
+    const getProducts = async () => {
+      const res = await fetch("/api/products");
+      const data = await res.json();
+      setProducts(data);
+      setLoading(false);
+    };
+
+    getProducts();
+  }, []);
   return <div>Products page</div>;
 };
 
