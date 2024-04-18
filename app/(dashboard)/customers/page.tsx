8d6773e1047c7ca1a7e015065a4ea8dcc5efd9c4
surajjayraman <suraj.jayraman@gmail.com>
2024-04-18 18:42:16 -0400

    Update customers page

--- app/(dashboard)/customers/page.tsx
+++ app/(dashboard)/customers/page.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import { DataTable } from "@/components/custom ui/DataTable";
+import { columns } from "@/components/customers/CustomerColumns";
+import { Separator } from "@/components/ui/separator";
+import Customer from "@/lib/models/Customer";
+import { connectToDB } from "@/lib/mongoDB";
 
-const page = () => {
+const Customers = async () => {
+  await connectToDB();
+
+  const customers = await Customer.find().sort({ createdAt: "desc" });
+
   return (
-    <div>page</div>
-  )
-}
+    <div className="px-10 py-5">
+      <p className="text-heading2-bold">Customers</p>
+      <Separator className="bg-grey-1 my-5" />
+      <DataTable columns={columns} data={customers} searchKey="name" />
+    </div>
+  );
+};
 
-export default page
+export const dynamic = "force-dynamic";
+
+export default Customers;
